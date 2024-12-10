9f0fed4c085d86ce417982ee4818988bbb4cc051
silvericarus <agonzalezrosa@hotmail.com>
2024-12-10 16:45:49 +0100

    Alignment error fixed

--- Grenade/grenade_launcher.gd
+++ Grenade/grenade_launcher.gd
@@ -1,19 +1,11 @@
 extends Node2D
 
-@onready var gun_collision : CollisionPolygon2D = $GunPoint
-@onready var gun_pivot : Sprite2D = $Sprite2D
+@onready var grenade : PackedScene = preload("res://Grenade/grenade.tscn")
+
+var gunpoint : Vector2
 
 func _ready() -> void:
-	#Where's the error??
-	gun_collision.position = Vector2.ZERO + Vector2(7, 2)
-	print(gun_collision.position)
-	gun_pivot.position = Vector2.ZERO + Vector2(7, 2)
-	gun_collision.rotation = 0
-	gun_pivot.rotation = 0
+	gunpoint = Vector2(-80, -16)
 
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
-	gun_collision.position = Vector2.ZERO + Vector2(7, 2)
-	gun_collision.rotation = 0
-	gun_pivot.position = Vector2.ZERO + Vector2(7, 2)
-	gun_pivot.rotation = 0
